255d678fcff61cabb4d2bc1fa28c18d81914e7cd
Soojeong Park <soojeong.park.ca@gmail.com>
2023-03-08 21:21:47 -0800

    add favicon, fix footer, and center App component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,23 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      Copyright &copy; 2023&nbsp;
-      <a href="https://twitter.com/codingsooj" target="_blank">
-        codingsooj
+      <div>
+        Copyright &copy; 2023&nbsp;
+        <a
+          className="twitterLink"
+          href="https://twitter.com/codingsooj"
+          target="_blank"
+        >
+          codingsooj
+        </a>
+      </div>
+      <a
+        className="diceFaviconCopy"
+        href="https://www.flaticon.com/free-icons/dice"
+        title="dice icons"
+        target="_blank"
+      >
+        Dice favicon created by bearicons - Flaticon
       </a>
     </footer>
   );
